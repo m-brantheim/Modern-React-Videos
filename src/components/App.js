e6075bd83bf204493b1e12c58861b87b13e52008
Magnus Brantheim <m.brantheim@gmail.com>
2019-10-29 12:21:53 +0100

    Added VideoDetail component and localStorage caching to search

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,57 @@ import React from 'react'
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 class App extends React.Component {
-    state = { videos: [] }
+    state = { videos: [], selectedVideo: null }
+
+    componentDidMount() {
+        this.onTermSubmit('fast cars')
+    }
 
     onTermSubmit = async term => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
+        const cachedResult = JSON.parse(localStorage.getItem(term))
+        let items = []
+        if (cachedResult) {
+            items = cachedResult
+        } else {
+            const response = await youtube.get('/search', {
+                params: {
+                    q: term
+                }
+            })
+            items = response.data.items
+            localStorage.setItem(term, JSON.stringify(items))
+        }
+        
+        this.setState({
+            videos: items,
+            selectedVideo: items.length > 0 ? items[0] : null
         })
-        this.setState({ videos: response.data.items })
+    }
+
+    onVideoSelect = video => {
+        this.setState({ selectedVideo: video })
     }
 
     render() {
         return (
         <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit} />
-            <VideoList videos={this.state.videos} />
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail video={this.state.selectedVideo} />
+                    </div>
+                    <div className="five wide column">
+                        <VideoList
+                            videos={this.state.videos}
+                            onVideoSelect={this.onVideoSelect}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
         )
     }
